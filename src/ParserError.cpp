aac1f3ea0113b5129c60d3670175167a94ffe3ec
Louis-Antoine DE MOULINS DE ROCHEFORT <lde-moul@student.42.fr>
2021-01-29 17:53:19 +0100

    Show line number for syntax errors

--- src/ParserError.cpp
+++ src/ParserError.cpp
@@ -2,4 +2,6 @@
 
 #include <string>
 
+ParserError::ParserError() : std::runtime_error("syntax error at end of file") {}
+
 ParserError::ParserError(uint32_t lineNum) : std::runtime_error("syntax error at line " + std::to_string(lineNum)) {}
